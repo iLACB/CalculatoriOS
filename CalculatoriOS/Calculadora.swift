50f3ad58beab691e4030865344d9c071ba0aa9a2
LACB <luis.cruzbenitez@gmail.com>
2018-05-29 10:24:10 -0500

    Se modifico la clase Calculadora

--- CalculatoriOS/Calculadora.swift
+++ CalculatoriOS/Calculadora.swift
@@ -15,11 +15,12 @@ public class Calculadora {
     public var protoDelegate: GNControlFunctionProtocol?
     
     public func suma(x: Double, y: Double){
+        if self.protoDelegate == nil{
+            print("Debe asignar una valor a la variable 'protoDelegate'")
+            return
+        }
         let resultado = x+y
         if !x.isNaN && !y.isNaN{
-            if self.protoDelegate == nil{
-                print("Debe asignar una valor a la variable 'protoDelegate'")
-            }
             self.protoDelegate?.gnResult(value: resultado, type: .Suma)
         } else{
             self.protoDelegate?.gnMessageError(error: "Solo se aceptan numeros", type: .Suma)
@@ -27,39 +28,41 @@ public class Calculadora {
     }
     
     public func resta(x: Double, y: Double){
-        let resultado = x - y
-        if !x.isNaN && !y.isNaN{
-            if self.protoDelegate == nil{
-                print("Debe asignar una valor a la variable 'protoDelegate'")
+        if let dele = self.protoDelegate {
+            let resultado = x - y
+            if !x.isNaN && !y.isNaN{
+                dele.gnResult(value: resultado, type: .Resta)
+            } else {
+                dele.gnMessageError(error: "Solo se aceptan numeros", type: .Resta)
             }
-            self.protoDelegate?.gnResult(value: resultado, type: .Resta)
-        } else {
-            self.protoDelegate?.gnMessageError(error: "Solo se aceptan numeros", type: .Resta)
+        }else{
+            print("Debe asignar una valor a la variable 'protoDelegate'")
         }
     }
     
     public func multiplicacion(x: Double, y: Double){
-        let resultado = x * y
-        if !x.isNaN && !y.isNaN{
-            if self.protoDelegate == nil{
-                print("Debe asignar una valor a la variable 'protoDelegate'")
+        if let dele = self.protoDelegate {
+            let resultado = x * y
+            if !x.isNaN && !y.isNaN{
+                dele.gnResult(value: resultado, type: .Multiplicacion)
+            } else {
+                dele.gnMessageError(error: "Solo se aceptan numeros", type: .Multiplicacion)
             }
-            self.protoDelegate?.gnResult(value: resultado, type: .Multiplicacion)
-        } else {
-            self.protoDelegate?.gnMessageError(error: "Solo se aceptan numeros", type: .Multiplicacion)
+        }else{
+            print("Debe asignar una valor a la variable 'protoDelegate'")
         }
     }
     
     public func division(x: Double, y: Double){
-        let resultado = x/y
-        if y == 0 {
-            if self.protoDelegate == nil{
-                print("Debe asignar una valor a la variable 'protoDelegate'")
+        if let dele = self.protoDelegate{
+            let resultado = x/y
+            if y == 0 {
+                dele.gnMessageError(error: "\(Int(y)) Es un valor incorrecto para la division", type: .Division)
+            }else {
+                dele.gnResult(value: Double(resultado), type: .Division)
             }
-            self.protoDelegate?.gnMessageError(error: "\(Int(y)) Es un valor incorrecto para la division", type: .Division)
-        }
-        else {
-            self.protoDelegate?.gnResult(value: Double(resultado), type: .Division)
+        }else {
+            print("Debe asignar una valor a la variable 'protoDelegate'")
         }
     }
 }
